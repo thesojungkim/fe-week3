a78e96391cbbc59796c53f5b4c02a5d6b8efdca4
SJPark <pcpark98@naver,com>
2022-05-10 00:33:37 +0900

    App.js 수정

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-// 해당 위치에 필요한 컴포넌트를 바탕으로 직접 google 검색 페이지를 개발하시면 됩니다!
+// 해당 위치에 필요한 컴포넌트를 바탕으로 직접 Bootstrap Album 페이지를 개발하시면 됩니다!
 function App() {
-  return <>google</>;
+  return <>따끔~~ 우리 팀 화이팅!!!</>;
 }
 
 export default App;
